1ff3872f885626239ce4f23079991046d063bd9e
Pavel <106407898+4head-ui@users.noreply.github.com>
2023-01-16 14:08:17 +0300

    Update settings.py

--- bboard/bboard/settings.py
+++ bboard/bboard/settings.py
@@ -158,7 +158,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = '**********************'
-EMAIL_HOST_PASSWORD = 'gtafeljjrmuciksh'
+EMAIL_HOST_PASSWORD = '****************'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
